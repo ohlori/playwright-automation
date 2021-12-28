2299338e000aae6f6d9b2266d81aaf65d23b7548
DESKTOP-DOQVB5C\cruzk <cruzkx@gmail.com>
2021-12-29 01:11:27 +0800

    Config.js and gitignore update

--- playwright.config.js
+++ playwright.config.js
@@ -1,22 +1,19 @@
 // playwright.config.js
 var fs = require("fs");
 var cake = fs.readFileSync("./cake.txt", "utf-8");
-// console.log(text);
+// console.log(cake);
 
-exports.config = {
+const config = {
   // Look for test files in the "tests" directory, relative to this configuration file
-  testDir: './tests',
+  testDir: 'tests',
   testMatch: ["**/__tests__/**/*.+(ts|js)", "**/?(*.)+(spec|test).+(ts|js)"],
-
-  globalSetup: './tsconfig.json',
-
+  
   use: {
     browserName: 'chromium',
     //browserName: 'firefox',
     //browserName: 'webkit',
 
     baseURL: "https://seller.shopee.ph",
-    
     extraHTTPHeaders: {
       "cookie": cake
     },
