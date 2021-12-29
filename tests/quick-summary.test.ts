e1979e0dfeff5fdc1ff74e07087666c67eb479e9
DESKTOP-DOQVB5C\cruzk <cruzkx@gmail.com>
2021-12-29 18:05:54 +0800

    [Quick Summary] Added additional information

--- tests/quick-summary.test.ts
+++ tests/quick-summary.test.ts
@@ -1,21 +1,40 @@
 import test from '@playwright/test';
+import { join } from 'path';
+const path = require('path');
+import { Base }  from "../util/base";
 var fs = require("fs");
+
+const base = new Base();
 
 test("Quick Summary", async ({ request, baseURL }) => {
     const res_ = await request.post(baseURL + "/api/v3/order/get_shipment_meta_multi_shop", {
         data: {
-            "orders": [
-                {
-                    "shop_id": 271248938,
-                    "region_id": "PH"
-                }
-            ]
+            "orders": [{"shop_id": 271248938,"region_id": "PH"}]
         }
     });
-    
-    await fs.writeFile ("./result/quick-summary.json", JSON.stringify(await res_.json(),undefined,2), function(err) {
-        if (err) throw err;
-            console.log('complete');
+    const data = await base.locateJSON(await res_.json(), "data.shop_list");
+    const res2 = await request.post(baseURL + "/api/v3/order/get_order_meta_multi_shop", {
+        data: {
+            "shop_list":[{"shop_id":271248938,"region_id":"PH"}]
         }
-    );
+    });
+
+    const combinedResponses = (JSON.stringify(await data[0],undefined,2)+ await JSON.stringify(await res2.json(),undefined,2)).replace(/\s}{/g,",");
+    await fs.writeFile ("./result/quick-summary.json", combinedResponses, async function(err) {
+        if (err) throw err;
+
+        const info = await base.getJSONData("/result/quick-summary.json",);
+        console.log("-------------------------------------------");
+        console.log("|              QUICK SUMMARY              |");
+        console.log("-------------------------------------------");
+        console.log("\tUNPAID / ABANDONED ORDER: " + info.data.unpaid);
+        
+        console.log("\n\tTOTAL TO SHIP: " + info.data.toship);
+        console.log("\x1b\t[33m%s\x1b[0m"," ► Unprocessed: " + info.data.toship_unprocessed);
+        console.log("\t ► Processed: " + info.data.toship_processed);
+        console.log("\n\tTOTAL IN SHIPPING: " + info.data.shipping);
+        console.log("\t ► In Transit: " + info.in_transit);
+        console.log("\t ► Delivered: " + (info.data.shipping - info.in_transit));
+        console.log("-------------------------------------------");
+    });
 })
--- util/base.ts
+++ util/base.ts
@@ -6,24 +6,57 @@ export class Base {
     public contentPath: string;
     public contentData: JSON;
 
+
+    public getContentData(): JSON {
+        return this.contentData;
+    }
+
     /**
      * Loads the content file by parsing the json file from the given @param {string} path.
      * @todo make the json reading async if the json files gets huge.
      */
-     public loadContent(path: string) {
-        const localPath = join(process.cwd(), path);
-        this.contentPath = normalizer.normalize(localPath);
-        this.contentData = JSON.parse(fs.readFileSync(this.contentPath, "utf8"), function(err) {
-            if (err) throw err;
-                console.log('complete');
-                return this.contentData;
-            }
-        );
+    public loadContent(path: string) {
+        try {
+            const localPath = join(process.cwd(), path);
+            this.contentPath = normalizer.normalize(localPath);
+            this.contentData = JSON.parse(fs.readFileSync(this.contentPath, "utf8"));
+            return this.contentData;
+        } catch (error) {
+            console.log("ERROR: unable to read content file.");
+        }
     }
 
+     public locateJSON(data: JSON, jsonStrc: string) {
+        let loc: JSON = data;
+        // split the received locator path in json with .
+        let str;
+        if (jsonStrc.includes(".")) {
+            str = jsonStrc.split(".");
 
-    public async getData(path: string, blockPath?: string): Promise <string> {
+            for (let i = 0; i < str.length; i++) {
+                loc = loc[str[i]];
+            }
+            return JSON.parse(JSON.stringify(loc,undefined,2));
+        } else {
+            return loc[jsonStrc];
+        }
+    }
+
+    public async getJSONData(path: string, block?: string): Promise <any> {
         let data = await this.loadContent(path);
-        return data["block"];
+        data = block === undefined ? data: this.locateJSON(data, block) ;
+        return data;
+    }
+
+    public async processOrderBody(infos: any): Promise <any> {    
+        let viewData = { 
+            orders : [] 
+        };
+
+        await infos.forEach (async function(column) {
+            await viewData.orders.push(await column);
+        });
+
+        return await viewData;
     }
 }
