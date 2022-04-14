f4045cc8c4d0e7949cd3007aed51d545e3ee74eb
KX Cruz <cruzkx@gmail.com>
2022-04-15 04:23:42 +0800

    Flow start and clean up

--- tests/get-data.test.ts
+++ tests/get-data.test.ts
@@ -6,12 +6,6 @@ var fs = require("fs/promises");
 const base = new Base();
 const call = new Calls();
 
-
-// test("Get COMPLETED items", async ({ request, baseURL }) => {
-//     await call.getCompleted({ request, baseURL });
-// })
-
-
 test("Get all TO SHIP items", async ({ request, baseURL }) => {
     test.setTimeout(0);
     await call.getAllToShip({ request, baseURL });
@@ -22,7 +16,6 @@ test("Calculate the profit", async () => {
 })
 
 test("Save to database", async () => {
-    test.setTimeout(0);
     await call.saveToDB();
 })
 
@@ -35,7 +28,7 @@ test("DAILY NET REPORT", async () => {
     const info = await base.loadContent("/result/to-ship-total.json");
     const data =  await base.locateJSON(await info);
     
-    console.log("Daily Net Sales : ₱" + String(Number(await await data.orders.map(x => x.net).reduce((acc, x) => x+acc, 0)).toFixed(2)).replace(/\B(?=(\d{3})+(?!\d))/g, ","));
-    console.log("Daily Net Profit: ₱" + String(Number(await await data.orders.map(x => x.profit.total).reduce((acc, x) => x+acc, 0)).toFixed(2)).replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+    console.log("Daily Net Sales : " + await base.pesoFormat(Number(await await data.orders.map(x => x.net).reduce((acc, x) => x+acc, 0))));
+    console.log("Daily Net Profit: " + await base.pesoFormat(Number(await await data.orders.map(x => x.profit.total).reduce((acc, x) => x+acc, 0))));
 })
 
--- tests/get-shipping-status.test.ts
+++ tests/get-shipping-status.test.ts
@@ -19,6 +19,7 @@ test("Get SHIPPPING status", async ({ request, baseURL }) => {
 
 test("Shipping Status Summary", async ({ request, baseURL }) => {
     const info = await base.loadContent("/result/shipping-status.json");
+    let flow = await base.loadJSONData("/accounting/flow.json");
     const data =  await base.locateJSON(await info);
     const rts_total_count = await data.orders.filter(x => x.status === 203).length;
     const rts_total = await data.orders.filter(x => x.status === 203).map(x => x.net).reduce((acc, x) => x+acc, 0);
@@ -32,20 +33,20 @@ test("Shipping Status Summary", async ({ request, baseURL }) => {
     console.log("-------------------------------------------");
     console.log("|     SUMMARY OF SHIPPING STATUS " + "[" + Object.keys(data.orders).length +"]"+"    |");
     console.log("-------------------------------------------");
-    console.log("\x1b[31m%s\x1b[0m","\t      RTS: " + rts_total_count + " | ₱" +  String(Number(await rts_total).toFixed(2)).replace(/\B(?=(\d{3})+(?!\d))/g, ","));;
+    console.log("\x1b[31m%s\x1b[0m","\t      RTS: " + rts_total_count + " | " +  await base.pesoFormat(Number(await rts_total)));
     console.log("-------------------------------------------");
-    console.log("\tDelivered:   " + delivered_count + " | ₱" +  String(Number(delivered_total).toFixed(2)).replace(/\B(?=(\d{3})+(?!\d))/g, ","));
-    console.log("\tIn-Progress: " + shipping_count +" | ₱" + String(Number(shipping_total).toFixed(2)).replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+    console.log("\tDelivered:   " + delivered_count + " | " +  await base.pesoFormat(Number(delivered_total)));
+    console.log("\tIn-Progress: " + shipping_count +" | " + await base.pesoFormat(Number(shipping_total)));
     console.log("-------------------------------------------");
     total = Number(Number(delivered_total) + Number(shipping_total));
-    console.log("\x1b[33m%s\x1b[0m","\tShipping Total  : ₱" + String(await total.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",")));
+    console.log("\x1b[33m%s\x1b[0m","\tShipping Total  : " + await base.pesoFormat(total));
  
     // TO SHIP SUMMARY
     const data2 = await base.loadContent("/result/to-ship-total.json");
     const toship =  await base.locateJSON(await data2);
     
     const sales = Number(await await toship.orders.map(x => x.net).reduce((acc, x) => x+acc, 0));
-    console.log("\x1b[33m%s\x1b[0m","\tDaily Net Sales : ₱" + String(await sales.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",")));
+    console.log("\x1b[33m%s\x1b[0m","\tDaily Net Sales : " + await base.pesoFormat(await sales));
     total = Number(await total + await sales);
     
     // CURRENT BALANCE, SUBTOTAL & UNCATEGORIZED STATUS CODE
@@ -54,16 +55,17 @@ test("Shipping Status Summary", async ({ request, baseURL }) => {
     const res_ = await request.get(baseURL + "/api/v3/finance/get_wallet_status/?wallet_type=0&bank_account_id=1021678");
     let rspn = await JSON.parse(JSON.stringify(await res_.json()));
     balance = rspn.data.balance;
-    console.log("\x1b[33m%s\x1b[0m","\tCurrent Balance : ₱" + Number(balance).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+    console.log("\x1b[33m%s\x1b[0m","\tCurrent Balance : " + await base.pesoFormat(Number(balance)));
     console.log("-------------------------------------------");
-    console.log("\x1b[32m%s\x1b[0m","         SUB-TOTAL : ₱" + Number(balance + total).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+    console.log("\x1b[33m%s\x1b[0m","\tSUB-TOTAL       : " + await base.pesoFormat(Number(balance + total)));
+    console.log("-------------------------------------------");
+    console.log("\x1b[32m%s\x1b[0m","\tON HAND         : " + await base.pesoFormat((balance + Number(flow.start.onHand)) - Number(flow.expense.map(x => x.amount).reduce((acc, x) => x+acc, 0))));
     console.log("-------------------------------------------"); 
     console.log("\x1b[34m%s\x1b[0m","\tUNCATEGORIZED STATUS CODE: " + (Object.keys(data.orders).length-(rts_total_count+delivered_count+shipping_count)));
     console.log("-------------------------------------------");
 })
 
 test.skip("Test", async ({ request, baseURL }) => {
-    let num: number[] = [5, 3, 2];
-    //console.log("\x1b[32m%s\x1b[0m","         SUB-TOTAL : ₱" + Number(3).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ","));
-    console.log(num);
+    let flow = await base.loadJSONData("/accounting/flow.json");
+    console.log(flow.expense.map(x => x.amount).reduce((acc, x) => x+acc, 0));
 })
